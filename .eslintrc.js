4018b9c4ffa64cb4a99f193bc6ba34c0abbab61f
manhhuy01 <manhhuy0112@gmail.com>
2020-12-05 01:38:39 +0700

    init socket

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,26 @@
 module.exports = {
-    "extends": "airbnb-base"
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": 0,
+    }
 };
--- index.js
+++ index.js
@@ -1,13 +1,77 @@
-const PORT = process.env.PORT || 5000
+// eslint-disable-next-line no-undef
+/*global require, process, console*/
+const PORT = process.env.PORT || 5000;
 
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const app = express();
 
-const app = express()
-app.use(express.json())
-app.use(cors())
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST', 'DELETE']
+  }
+});
 
-app.get('/', (req, res) => res.send('welcome'))
+
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => res.send('welcome'));
+
+let players = [];
+let rooms = [
+  {
+    id: 1,
+    maxPlayer: 4,
+    players: [],
+    isFull: false,
+  }
+];
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  io.emit('roomInfo', { totalPlayer: players.length, rooms });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    players = players.filter(s => s.id !== socket.id);
+    io.emit('roomInfo', { totalPlayer: players.length, rooms });
+    rooms.forEach(room => {
+      const idPlayer = room.players.find(id => socket.id === id);
+      if(idPlayer){
+        room.isFull = false;
+        room.players = room.players.filter(id => id !== idPlayer);
+        io.emit('roomInfo', { totalPlayer: players.length, rooms });
+      }
+    });
+  });
+
+  socket.on('action', ({ type, data }) => {
+    switch (type) {
+    case 'join':{
+      let room = rooms.find(x => x.id === data.id);
+      if (room &&  room.players.length < room.maxPlayer) {
+        console.log('user join room ', data.id);
+        socket.join(`room.${data.id}`);
+        socket.to(`room.${data.id}`).emit('notification', `player ${socket.id} has joined`);
+        io.to(socket.id).emit('join', room);
+        room.players.push(socket.id);
+        if(room.players.length === room.maxPlayer){
+          room.isFull = true;
+        }
+      }
+      io.emit('roomInfo', { totalPlayer: players.length, rooms });
+
+      break;
+    }
+    default:
+      break;
+    }
 
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+  });
+});
+
+http.listen(PORT, () => console.log(`Listening on ${PORT}`));
