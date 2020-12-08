79a40cb0e806212bcd6c7e31a902144edd0af740
manhhuy01 <manhhuy0112@gmail.com>
2020-12-09 00:52:12 +0700

    change api

--- index.js
+++ index.js
@@ -93,116 +93,128 @@ io.on('connection', (socket) => {
       console.log(`game at room ${room.id} started`);
       break;
     }
-    case 'collect_token': {
-      // bốc token thường
-      /* 
-              tokens: {
-                COLOR.BLACK: 1,
-                COLOR.BLUE: 1,
-                COLOR.RED: 1,
-              }
-              or 
-              tokens: {
-                COLOR.BLACK: 2,
-              }
-              */
-      if (!gameHandler.validateRoomAndTurn(rooms, socket.id, data.roomId)) return;
-      const {
-        roomId,
-        token
-      } = data;
-        // get data
-      const room = rooms.find(x => x.id === roomId);
-      const user = room.players.find(x => x.socketId === socket.id);
-      const player = room.game.players[user.turn];
-      if (gameHandler.sumToken(player.token) > 10) return;
-
-      let tokenNames = Object.keys(token);
-      if (!tokenNames.length) return;
-      if (tokenNames[COLOR.GOLD] > 0) return;
-
-      if (tokenNames.length > 3) return;
-      if (tokenNames.length > 1) {
-        let valid = true;
-        tokenNames.forEach(x => {
-          if (token[x] > 1) valid = false;
-        });
-        if (!valid) return;
-      }
-      if (tokenNames.length == 1 && token[tokenNames[0]] > 2) return;
-
-      // it's ok input, validate table
-      let valid = false;
-      tokenNames.forEach(color => {
-        if (token[color] === 1 && room.game.table.token[color] > 1) valid = true;
-        if (token[color] === 2 && room.game.table.token[color] > 3) valid = true;
-      });
-      if (!valid) return;
-
-      // everything ok, update game
-      player.token = gameHandler.addToken(player.token, token);
-      room.game.table.token = gameHandler.removeToken(room.game.table.token, token);
-
-      if (gameHandler.sumToken(player.token) > 10) return;
-
-      room.game.currentTurn += 1;
-      if (room.game.currentTurn > room.game.players.length) room.game.currentTurn = 1;
-
-      // noti
-      io.to(`room.${data.roomId}`).emit(`room.${data.roomId}.info`, room);
-      break;
-    }
-    case 'return_token': {
-      const { roomId, token } = data;
-      if (!gameHandler.validateRoomAndTurn(rooms, socket.id, data.roomId)) return;
-      // get data
-      const room = rooms.find(x => x.id === roomId);
-      const user = room.players.find(x => x.socketId === socket.id);
-      const player = room.game.players[user.turn];
-
-      if (gameHandler.sumToken(player.token) < 10) return;
-      if (gameHandler.sumToken(token) <= 0) return;
-
-      // everything ok, update game
-      player.token = gameHandler.removeToken(player.token, token);
-      room.game.table.token = gameHandler.addToken(room.game.table.token, token);
-
-      // noti
-      io.to(`room.${data.roomId}`).emit(`room.${data.roomId}.info`, room);
-      break;
-    }
-    case 'deposit_card': {
-      // đặt cọc
-      break;
-    }
-    case 'buy_card': {
-      // mua thẻ trên tay hoặc thẻ dưới bàn
-      break;
-    }
-    case 'throw_turn': {
-      const { roomId } = data;
-
-      if (!gameHandler.validateRoomAndTurn(rooms, socket.id, data.roomId)) return;
-      const room = rooms.find(x => x.id === roomId);
-      const user = room.players.find(x => x.socketId === socket.id);
-      const player = room.game.players[user.turn];
-
-      if (gameHandler.sumToken(player.token) > 10) return;
-      // không làm gì cả, bỏ lượt
-      
-      room.game.currentTurn += 1;
-      if (room.game.currentTurn > Object.keys(room.game.players).length) room.game.currentTurn = 1;
-      // noti
-      io.to(`room.${data.roomId}`).emit(`room.${data.roomId}.info`, room);
-
-      break;
-    }
+    // case 'deposit_card': {
+    //   // đặt cọc
+    //   break;
+    // }
+    // case 'buy_card': {
+    //   // mua thẻ trên tay hoặc thẻ dưới bàn
+    //   break;
+    // }
+   
     default:
       break;
     }
 
 
   });
+});
+
+const sendBadRequest = res => {
+  res.status(400).send();
+};
+
+const sendOkRequest = res => {
+  res.status(200).send();
+};
+
+app.post('/collect_token', (req, res) => {
+  // bốc token thường
+  /* 
+  tokens: {
+    COLOR.BLACK: 1,
+    COLOR.BLUE: 1,
+    COLOR.RED: 1,
+  }
+  or 
+  tokens: {
+    COLOR.BLACK: 2,
+  }
+  */
+
+  const { socketId, roomId, token } = req.body;
+
+  if (!gameHandler.validateRoomAndTurn(rooms, socketId, roomId)) return sendBadRequest(res);
+
+  // get data
+  const room = rooms.find(x => x.id === roomId);
+  const user = room.players.find(x => x.socketId === socketId);
+  const player = room.game.players[user.turn];
+  if (gameHandler.sumToken(player.token) > 10) return sendBadRequest(res);
+
+  let tokenNames = Object.keys(token);
+  if (!tokenNames.length) return sendBadRequest(res);
+  if (tokenNames[COLOR.GOLD] > 0) return sendBadRequest(res);
+
+  if (tokenNames.length > 3) return sendBadRequest(res);
+  if (tokenNames.length > 1) {
+    let valid = true;
+    tokenNames.forEach(x => {
+      if (token[x] > 1) valid = false;
+    });
+    if (!valid) return sendBadRequest(res);
+  }
+  if (tokenNames.length == 1 && token[tokenNames[0]] > 2) return sendBadRequest(res);
+
+  // it's ok input, validate table
+  let valid = false;
+  tokenNames.forEach(color => {
+    if (token[color] === 1 && room.game.table.token[color] > 1) valid = true;
+    if (token[color] === 2 && room.game.table.token[color] > 3) valid = true;
+  });
+  if (!valid) return sendBadRequest(res);
+
+  // everything ok, update game
+  player.token = gameHandler.addToken(player.token, token);
+  room.game.table.token = gameHandler.removeToken(room.game.table.token, token);
+
+  if (gameHandler.sumToken(player.token) > 10) return sendBadRequest(res);
+
+  room.game.currentTurn += 1;
+  if (room.game.currentTurn > room.game.players.length) room.game.currentTurn = 1;
+
+  // noti
+  io.to(`room.${roomId}`).emit(`room.${roomId}.info`, room);
+  return sendOkRequest(res);
+});
+
+app.post('/return_token', (req, res)=> {
+  const { roomId, token, socketId } = req.body;
+  if (!gameHandler.validateRoomAndTurn(rooms, socketId, roomId)) sendBadRequest(res);
+  // get data
+  const room = rooms.find(x => x.id === roomId);
+  const user = room.players.find(x => x.socketId === socketId);
+  const player = room.game.players[user.turn];
+
+  if (gameHandler.sumToken(player.token) < 10) sendBadRequest(res);
+  if (gameHandler.sumToken(token) <= 0) sendBadRequest(res);
+
+  // everything ok, update game
+  player.token = gameHandler.removeToken(player.token, token);
+  room.game.table.token = gameHandler.addToken(room.game.table.token, token);
+
+  // noti
+  io.to(`room.${roomId}`).emit(`room.${roomId}.info`, room);
+  return sendOkRequest(res);
+});
+
+
+app.post('/throw_turn', (req, res) => {
+  const { socketId, roomId } = req.body;
+
+  if (!gameHandler.validateRoomAndTurn(rooms, socketId, roomId)) return sendBadRequest(res);
+  const room = rooms.find(x => x.id === roomId);
+  const user = room.players.find(x => x.socketId === socketId);
+  const player = room.game.players[user.turn];
+
+  if (gameHandler.sumToken(player.token) > 10) return sendBadRequest(res);
+  // không làm gì cả, bỏ lượt
+
+  room.game.currentTurn += 1;
+  if (room.game.currentTurn > Object.keys(room.game.players).length) room.game.currentTurn = 1;
+  // noti
+  io.to(`room.${roomId}`).emit(`room.${roomId}.info`, room);
+  return sendOkRequest(res);
 });
 
 http.listen(PORT, () => console.log(`Listening on ${PORT}`));
